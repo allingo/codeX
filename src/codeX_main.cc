10e85d2bc9becc8dec1a79c69628eb8d331a0e33
Allingo <466245501@qq.com>
2017-04-22 10:11:46 +0800

    Init the GL proj.

--- src/codeX_main.cc
+++ src/codeX_main.cc
@@ -1,10 +1,29 @@
 /*
- * Codema studio Copyright
  *
  */
-#include <stdio.h>
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
+using namespace std;
 
-int main()
+void display_version()
 {
+    //openGL生产商及版本
+    const char* vendorName = reinterpret_cast<const
+        char*>(glGetString(GL_VENDOR));
+    const char* version = reinterpret_cast<const
+        char*>(glGetString(GL_VERSION));
+    cout<<"vendorName:"<<vendorName<<endl;
+    cout<<"version:"<<version<<endl;
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow("GLEW Test");//get an openGL context
+
+    //openGL生产商及版本
+    display_version();
     return 0;
 }
