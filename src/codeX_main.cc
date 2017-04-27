bfddebc28c3bc2ce0c027398309f1efcb959bc47
Allingo <466245501@qq.com>
2017-04-27 08:54:23 +0800

    Add shader.

--- src/codeX_main.cc
+++ src/codeX_main.cc
@@ -15,12 +15,15 @@ using namespace std;
 void display_version()
 {
     //openGL生产商及版本
-    const char* vendorName = reinterpret_cast<const
-        char*>(glGetString(GL_VENDOR));
-    const char* version = reinterpret_cast<const
-        char*>(glGetString(GL_VERSION));
+    const GLubyte* vendorName = glGetString(GL_VENDOR);
+    const GLubyte* version = glGetString(GL_VERSION);
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    const GLubyte* slversion =
+        glGetString(GL_SHADING_LANGUAGE_VERSION);
     cout<<"vendorName:"<<vendorName<<endl;
     cout<<"version:"<<version<<endl;
+    cout<<"renderer:"<<renderer<<endl;
+    cout<<"GLSL:"<<slversion<<endl;
 }
 
 int main(int argc, char *argv[])
